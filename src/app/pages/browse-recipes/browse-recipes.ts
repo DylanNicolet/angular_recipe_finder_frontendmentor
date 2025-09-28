430904e89792011fb3834a75ae0705718d3efaa0
DylanLaptop <dylannicolet@hotmail.com>
2025-09-28 14:47:02 +0400

    [SETUP] Add partial filter logic, no styling

--- src/app/pages/browse-recipes/browse-recipes.ts
+++ src/app/pages/browse-recipes/browse-recipes.ts
@@ -1,24 +1,72 @@
 import { Component } from '@angular/core';
 import { RecipeCard } from "../../components/recipe-card/recipe-card";
 import { DataService } from '../../data';
-
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-browse-recipes',
-  imports: [RecipeCard],
-  templateUrl: './browse-recipes.html',
-  styleUrl: './browse-recipes.scss'
+    selector: 'app-browse-recipes',
+    standalone: true,
+    imports: [RecipeCard, ReactiveFormsModule],
+    templateUrl: './browse-recipes.html',
+    styleUrl: './browse-recipes.scss'
 })
 export class BrowseRecipes {
-  recipes: any[] = [];
+    recipes: any[] = [];
+    filteredRecipes: any[] = [];
 
-  constructor(private dataService: DataService) {}
+    // Filter input trackers
+    prepTime = new FormControl<number | null>(null);
+    cookTime = new FormControl<number | null>(null);
 
-  ngOnInit() {
-    this.dataService.getData().subscribe((data) => {
-      console.log(data);
-      
-      this.recipes = data;
-    });
-  }
+    constructor(private dataService: DataService) {}
+
+    ngOnInit() {
+        // Emulate data being fetch from API (currently JSON file in assets folder)
+        this.dataService.getData().subscribe((data) => {
+            this.recipes = data;
+            this.filteredRecipes = data;
+        });
+
+        // Track changes to Prep Time filter
+        this.prepTime.valueChanges.subscribe(value => {
+            if (value !== null) {
+                // Clear cookTime filter without triggering its subscription
+                this.cookTime.setValue(null, { emitEvent: false });
+            }
+
+            if (value === null) {
+                // Reset recipes
+                this.filteredRecipes = this.recipes;
+            } else {
+                // Filter the filteredRecipes array to update recipe list
+                this.filteredRecipes = this.recipes.filter(recipe => recipe.prepMinutes <= value);
+            }
+        });
+
+        // Track changes to Cook Time filter
+        this.cookTime.valueChanges.subscribe(value => {
+            if (value !== null) {
+                // Clear prepTime filter without triggering its subscription
+                this.prepTime.setValue(null, { emitEvent: false });
+            }
+
+            if (value === null) {
+                // Reset recipes
+                this.filteredRecipes = this.recipes;
+            } else {
+                // Filter the filteredRecipes array to update recipe list
+                this.filteredRecipes = this.recipes.filter(recipe => recipe.cookMinutes <= value);
+            }
+        });
+    }
+
+    // Reset prepTime filter
+    clearPrepTime() {
+        this.prepTime.setValue(null);
+    }
+
+    // Reset cookTime filter
+    clearCookTime() {
+        this.cookTime.setValue(null);
+    }
 }
