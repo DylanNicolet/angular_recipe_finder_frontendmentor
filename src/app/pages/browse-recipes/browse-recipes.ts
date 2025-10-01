c0515ab801a209f373d69d748249aed25094731b
DylanLaptop <dylannicolet@hotmail.com>
2025-10-01 16:29:14 +0400

    [SETUP] Added search input logic

--- src/app/pages/browse-recipes/browse-recipes.ts
+++ src/app/pages/browse-recipes/browse-recipes.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RecipeCard } from "../../components/recipe-card/recipe-card";
 import { DataService } from '../../data';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
@@ -14,6 +14,11 @@ export class BrowseRecipes {
     recipes: any[] = [];
     filteredRecipes: any[] = [];
 
+    isSearchActive = signal<boolean>(false);
+
+    // Search input
+    searchInput = new FormControl<string>('');
+
     // Filter input trackers
     prepTime = new FormControl<number | null>(null);
     cookTime = new FormControl<number | null>(null);
@@ -27,6 +32,36 @@ export class BrowseRecipes {
             this.filteredRecipes = data;
         });
 
+        // Track changes to Search input field
+        this.searchInput.valueChanges.subscribe(value => {
+            // Reset both filters
+            this.prepTime.setValue(null, { emitEvent: false });
+            this.prepTime.setValue(null, { emitEvent: false });
+
+            if (!value) {
+                // Reset recipes
+                this.filteredRecipes = this.recipes;
+                this.isSearchActive.set(false);
+            } else {
+                const search = value.toString().toLowerCase();
+
+                this.filteredRecipes = this.recipes.filter(recipe => {
+                    // Does the search match any titles
+                    const inTitle = recipe.title.toLowerCase().includes(search);
+
+                    // Does the search match any ingredients
+                    const inIngredients = Array.isArray(recipe.ingredients) &&
+                        recipe.ingredients.some((ing: string) =>
+                            ing.toLowerCase().includes(search)
+                        );
+
+                    return inTitle || inIngredients;
+                });
+
+                this.isSearchActive.set(true);
+            }
+        });
+
         // Track changes to Prep Time filter
         this.prepTime.valueChanges.subscribe(value => {
             if (value !== null) {
@@ -38,8 +73,13 @@ export class BrowseRecipes {
                 // Reset recipes
                 this.filteredRecipes = this.recipes;
             } else {
-                // Filter the filteredRecipes array to update recipe list
-                this.filteredRecipes = this.recipes.filter(recipe => recipe.prepMinutes <= value);
+                if (this.isSearchActive()) { // If a search is already active
+                    // Filter the filteredRecipes (already searched) array to update recipe list
+                    this.filteredRecipes = this.filteredRecipes.filter(recipe => recipe.prepMinutes <= value);
+                } else {
+                    // Filter the original recipes array to update recipe list
+                    this.filteredRecipes = this.recipes.filter(recipe => recipe.prepMinutes <= value);
+                }
             }
         });
 
@@ -54,8 +94,13 @@ export class BrowseRecipes {
                 // Reset recipes
                 this.filteredRecipes = this.recipes;
             } else {
-                // Filter the filteredRecipes array to update recipe list
-                this.filteredRecipes = this.recipes.filter(recipe => recipe.cookMinutes <= value);
+                if (this.isSearchActive()) { // If a search is already active
+                    // Filter the filteredRecipes (already searched) array to update recipe list
+                    this.filteredRecipes = this.filteredRecipes.filter(recipe => recipe.cookMinutes <= value);
+                } else {
+                    // Filter the original recipes array to update recipe list
+                    this.filteredRecipes = this.recipes.filter(recipe => recipe.cookMinutes <= value);
+                }
             }
         });
     }
