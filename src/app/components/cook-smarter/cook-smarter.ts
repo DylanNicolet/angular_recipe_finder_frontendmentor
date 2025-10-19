aef7f158d22851de207927e3ee61905b9e53ad4a
Dylan Nicolet <megadylan1@hotmail.com>
2025-10-19 22:00:42 +0400

    [FIX] Fixed Header and replaced all href with routerLinks

--- src/app/components/cook-smarter/cook-smarter.ts
+++ src/app/components/cook-smarter/cook-smarter.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cook-smarter',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cook-smarter.html',
   styleUrl: './cook-smarter.scss'
 })
